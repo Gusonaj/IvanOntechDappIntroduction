ba427efd8b2b80654beb808c97c6b3986d582caa
Gusonaj <67588245+Gusonaj@users.noreply.github.com>
2020-07-07 16:16:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-var web3 = new Web3(Web3.givenProvider);
+if(typeof web3 != 'undefined'){
+  web3 = new Web3(web3.currentProvider);
+  } else {
+  web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
+  }
 var contractInstance;
 
 $(document).ready(function() {
